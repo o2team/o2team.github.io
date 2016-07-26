032eebe5e9a6b30e319de1020799eed4303cd681
LV <mamboer@live.com>
2016-07-26 19:10:09 +0800

    update travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-# Decrypt the private key
-openssl aes-256-cbc -K $encrypted_7bc606770d22_key -iv $encrypted_7bc606770d22_iv -in .travis/o2-site.enc -out ~/.ssh/o2-site -d
-# Set the permission of the key
-chmod 600 ~/.ssh/o2-site
-# Start SSH agent
-eval $(ssh-agent)
-# Add the private key to the system
-ssh-add ~/.ssh/o2-site
-# Copy SSH config
-cp .travis/ssh_config ~/.ssh/config
-# Set Git config
-git config --global user.name "AOTU Bot"
-git config --global user.email o2labs@qq.com
-# Clone the repository
-git clone git@o2-site.github.com:o2team/o2team.github.io.git .deploy_git
-# Checkout the master branch (Default is v2)
-cd .deploy_git
-git checkout master
-# Deploy to GitHub
-cd ..
-npm run deploy
+if [ "$1" = "false" ]
+# not a pull request, deploy to github pages
+then ( cd public
+  git init
+  git config user.name "AOTULabs"
+  git config user.email "o2labs@qq.com"
+  git add .
+  git commit -m "Deployed from Travis CI"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REPO}" master:master
+)
+fi
