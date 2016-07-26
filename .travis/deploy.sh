8a00fda9fa2c7bad9478dced28132b94f927f4ac
LV <mamboer@live.com>
2016-07-26 10:16:21 +0800

    add travis configs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_7bc606770d22_key -iv $encrypted_7bc606770d22_iv in o2team_o2team-github-io.enc -out ~\/.ssh/o2team_o2team-github-io -d
+openssl aes-256-cbc -K $encrypted_7bc606770d22_key -iv $encrypted_7bc606770d22_iv in o2-site.enc -out ~\/.ssh/o2-site -d
 # Set the permission of the key
-chmod 600 ~/.ssh/o2team_o2team-github-io
+chmod 600 ~/.ssh/o2-site
 # Start SSH agent
 eval $(ssh-agent)
 # Add the private key to the system
-ssh-add ~/.ssh/o2team_o2team-github-io
+ssh-add ~/.ssh/o2-site
 # Copy SSH config
 cp .travis/ssh_config ~/.ssh/config
 # Set Git config
 git config --global user.name "AOTU Bot"
 git config --global user.email o2labs@qq.com
 # Clone the repository
-git clone git@o2team.github.com:o2team/o2team.github.io.git .deploy_git
+git clone git@o2-site.github.com:o2team/o2team.github.io.git .deploy_git
 # Deploy to GitHub
 npm run deploy
