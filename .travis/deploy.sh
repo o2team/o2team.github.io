1fec8026db1b4194860c860b4bbd2a76f262300f
LV <mamboer@live.com>
2016-07-26 18:21:04 +0800

    add npm run clean command

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_7bc606770d22_key -iv $encrypted_7bc606770d22_iv -in o2-site.enc -out ~\/.ssh/o2-site -d
+openssl aes-256-cbc -K $encrypted_7bc606770d22_key -iv $encrypted_7bc606770d22_iv -in .travis/o2-site.enc -out ~\/.ssh/o2-site -d
 # Set the permission of the key
 chmod 600 ~/.ssh/o2-site
 # Start SSH agent
@@ -14,5 +14,9 @@ git config --global user.name "AOTU Bot"
 git config --global user.email o2labs@qq.com
 # Clone the repository
 git clone git@o2-site.github.com:o2team/o2team.github.io.git .deploy_git
+# Checkout the master branch (Default is v2)
+cd .deploy_git
+git checkout master
 # Deploy to GitHub
+cd ..
 npm run deploy
